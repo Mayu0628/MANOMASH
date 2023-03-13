021a916e225d1a6f22923eab4fe4e7a1c1da8d8d
mahiro <c1102864@planet.kanazawa-it.ac.jp>
2023-03-14 07:34:34 +0900

    クッキーを使用したマイページの表示、編集、自作関数の説明を表記

--- handler/mypage_handler.go
+++ handler/mypage_handler.go
@@ -1,10 +1,38 @@
 package handler
 
 import (
-	"io"
+	"MANOMASH/database"
+
 	"net/http"
+	"log"
+	"fmt"
+	"encoding/json"
 )
 
 func MyPageHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "FetchProfileHandler\n")
+	cookies := req.Cookies()
+	type User struct{
+		Id int
+		UserName string
+		Email string
+		Password string
+		Introduce string
+	}
+	if cookies != nil{
+    for _, c := range cookies {
+		var result User
+        email := c.Value
+		log.Print("Name:", c.Name, "Value:", c.Value)
+		database.DB.Raw("SELECT id, user_name, email, password, introduce FROM users WHERE email = ?", email).Scan(&result) // (*sql.Row)
+		fmt.Println(result)
+		fmt.Println(result.Email)
+		ResData:= ResFlgCreate(1,"succesful")
+		json.NewEncoder(w).Encode(ResData)
+		json.NewEncoder(w).Encode(result)
+		return
+    }
+	}
+	fmt.Print("クッキーがないです")
+	ResData:= ResFlgCreate(0,"fail")
+	json.NewEncoder(w).Encode(ResData)
 }
