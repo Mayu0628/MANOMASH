498aaa318fa3ef4b11b79df42aab0f5523cd93f5
mahiro <c1102864@planet.kanazawa-it.ac.jp>
2023-03-13 15:01:22 +0900

    保存用

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"MANOMASH/database"
-	//"MANOMASH/hundler"
+	//"MANOMASH/controller"
+	//"MANOMASH/router"
 	"fmt"
 	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
@@ -31,7 +33,38 @@ func main() {
 		fmt.Println("データベース接続成功")
 	}
 
+	
 	fmt.Println("hallo,world")
 	handler := c.Handler(r)
 	http.ListenAndServe(":8080", handler)
 }
+
+//func main() {
+//	//c := cors.New(cors.Options{
+//	//	AllowedOrigins:   []string{"http://localhost:8080"},
+//	//	AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodDelete, http.MethodOptions},
+//	//	AllowedHeaders:   []string{"*"},
+//	//	AllowCredentials: true,
+//	//})
+//	database.GormConnect()
+//	sqlDB := database.DB
+//	db := sqlDB.DB()
+//	defer db.Close()
+//
+//	err := db.Ping()
+//
+//	if err != nil {
+//		fmt.Println("エラー")
+//		fmt.Println(err)
+//		return
+//	} else {
+//		fmt.Println("データベース接続成功")
+//	}
+//
+//	fmt.Println("hallo,world")
+//	//con := controller.NewMyAppController(sqlDB)
+//
+//	//r := router.NewRouter(con)
+//	//http.ListenAndServe(":8080", r)
+//}
+//
