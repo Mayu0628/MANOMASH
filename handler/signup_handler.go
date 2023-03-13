ec05f7a57fd930e481e2692734ad8b5ee3c8da26
mahiro <c1102864@planet.kanazawa-it.ac.jp>
2023-03-13 21:31:08 +0900

    サインアップとレスポンスの設定

--- handler/signup_handler.go
+++ handler/signup_handler.go
@@ -3,8 +3,46 @@ package handler
 import (
 	"io"
 	"net/http"
+	"fmt"
+	"log"
+	"encoding/json"
+	"MANOMASH/model"
+	"MANOMASH/database"
 )
 
 func SignUpHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "FetchProfileHandler\n")
+	var reqUserData model.User
+	var ResData model.ResFlg
+	fmt.Println("サインアップスタート")
+	if err := json.NewDecoder(req.Body).Decode(&reqUserData); err != nil {
+		fmt.Println(err)
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+	user := model.User {
+		UserName : reqUserData.UserName,
+		Email : reqUserData.Email,
+		Password : reqUserData.Password,
+	}
+	result := database.DB.First(&user, "email = ?", reqUserData.Email)
+	if result.Error != nil{
+		result = database.DB.Create(&user)
+		ResData.Status = 1
+		ResData.Result = "success"
+		if result.Error != nil{
+			log.Fatal(result.Error)
+		}
+		json.NewEncoder(w).Encode(reqUserData)
+		fmt.Println(reqUserData)
+		if err := json.NewEncoder(w).Encode(ResData); err != nil{
+			fmt.Println(err)
+		}
+		fmt.Println( ResData)
+		io.WriteString(w, "アカウントが作成されました\n")
+
+		return
+	}
+	ResData.Status = 0
+	ResData.Result = "fail"
+	io.WriteString(w, "そのメールアドレスは既に使用されています\n")
+	json.NewEncoder(w).Encode(ResData)
 }
--- main.go
+++ main.go
@@ -7,32 +7,32 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/rs/cors"
+	//"github.com/rs/cors"
 )
 
 func main() {
 	r := mux.NewRouter()
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:8080"},
-		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodDelete, http.MethodOptions},
-		AllowedHeaders:   []string{"*"},
-		AllowCredentials: true,
-	})
+	//c := cors.New(cors.Options{
+	//	AllowedOrigins:   []string{"http://localhost:8080"},
+	//	AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodDelete, http.MethodOptions},
+	//	AllowedHeaders:   []string{"*"},
+	//	AllowCredentials: true,
+	//})
 	database.GormConnect()
 	sqlDB, _ := database.DB.DB()
 	defer sqlDB.Close()
 
 	fmt.Println("hallo,world")
-	r.HandleFunc("/login", handler.LoginHandler).Methods(http.MethodGet)
-	r.HandleFunc("/sign-up", handler.SignUpHandler).Methods(http.MethodGet)
-	r.HandleFunc("/mypage/edit",handler.MyPageEditHandler).Methods(http.MethodGet)
-	r.HandleFunc("/mypage",handler.MyPageHandler).Methods(http.MethodPost)
-	r.HandleFunc("/profile", handler.ProfileHandler).Methods(http.MethodPost)
-	r.HandleFunc("/profile/add", handler.ProfileAddHandler).Methods(http.MethodGet)
-	r.HandleFunc("/profile/edit", handler.ProfileEditHandler).Methods(http.MethodGet)
+	r.HandleFunc("/login", handler.LoginHandler).Methods(http.MethodPost)
+	r.HandleFunc("/sign-up", handler.SignUpHandler).Methods(http.MethodPost)
+	r.HandleFunc("/mypage/edit",handler.MyPageEditHandler).Methods(http.MethodPost)
+	r.HandleFunc("/mypage",handler.MyPageHandler).Methods(http.MethodGet)
+	r.HandleFunc("/profile", handler.ProfileHandler).Methods(http.MethodGet)
+	r.HandleFunc("/profile/add", handler.ProfileAddHandler).Methods(http.MethodPost)
+	r.HandleFunc("/profile/edit", handler.ProfileEditHandler).Methods(http.MethodPost)
 	r.HandleFunc("/profile/delete", handler.ProfileDeleteHandler).Methods(http.MethodDelete)
-	r.HandleFunc("toppage", handler.TopPageHandler).Methods(http.MethodPost)
+	r.HandleFunc("/toppage", handler.TopPageHandler).Methods(http.MethodGet)
 
-	handler := c.Handler(r)
-	http.ListenAndServe(":8080", handler)
+	//handler := c.Handler(r)
+	http.ListenAndServe(":8080", r)
 }
