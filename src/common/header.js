de6a340af01c5eec4ecdfa835d1332c621d98ba9
nonokaM <c1201071@gmail.com>
2023-03-13 15:01:58 +0900

    ヘッダーを全ページに追加

---
+++ src/common/header.js
@@ -0,0 +1,16 @@
+document.getElementById("header").innerHTML = `
+<header>
+    <div class="left-wrapper">
+        <img class="osiprofile-icon" src="images/osiprofile-icon.PNG">
+        <img class="osiprofile-logo"src="images/osiprofile-logo.PNG">
+    </div>
+    <div class="right-wrapper">
+        <ul>
+            <li><a href="mypage.html">マイページ</a></li>
+            <li><a href=""></a>検索</li>
+            <li><a href="profile-add.html">＋新規作成</a></li>
+            <li class="login-btn"><a class=login-link href="login.html">ログイン</a></li>
+        </ul>
+    </div>
+</header>
+`
