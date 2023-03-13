d97f904f75d917fd62b8c47acf1ae4baddbe541e
nonokaM <c1201071@gmail.com>
2023-03-14 00:32:44 +0900

    全ページ共通のフッターを追加

---
+++ src/common/footer.js
@@ -0,0 +1,10 @@
+document.getElementById("footer").innerHTML = `
+<head>
+    <link rel="stylesheet" href="../images/MANOMASH-logo.PNG">
+    <link rel="stylesheet" href="../css/footer.css">
+</head>
+<footer>
+    <img src="images/MANOMASH-logo.PNG" alt="">
+    <p>@KIT_Smapro</p> 
+</footer>
+`
