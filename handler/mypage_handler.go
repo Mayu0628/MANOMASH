9517f19a3ca7415e8a5be81d0285cffc828fea34
mahiro <c1102864@planet.kanazawa-it.ac.jp>
2023-03-15 00:02:45 +0900

    a

--- handler/mypage_handler.go
+++ handler/mypage_handler.go
@@ -31,7 +31,7 @@ func MyPageHandler(w http.ResponseWriter, req *http.Request) {
 	var OshiData model.Oshi
 	database.DB.First(&SendID, "user_id = ?", id)
 	database.DB.First(&OshiData, "user_id = ?", id)
-	for i := 0; i < 3; i++ {
+	for i := 1; i < 4; i++ {
 		database.DB.First(&OshiData, "oshi_id = ?", i)
 		Response.OshiName[i] = OshiData.OshiName
 	Response.Status, Response.Result, Response.UserID, Response.UserName, Response.Introduce = 1, "succesful", id, SendID.UserName, SendID.Introduce
