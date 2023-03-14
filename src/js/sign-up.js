253b17f22ce27dab4c59754a0a9379faf71faa1f
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2023-03-14 21:13:27 +0900

    新規登録警告追加

--- src/js/sign-up.js
+++ src/js/sign-up.js
@@ -3,14 +3,18 @@ const getSignUpInfo = () => {
     const user_name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const pass = document.getElementById('pass').value;
+    const confirmation = document.getElementById('confirmation').value;
 
     console.log(user_name);
     console.log(email);
     console.log(pass);
+    console.log(confirmation);
+
     return {
         name: user_name,
         email: email,
-        password: pass
+        password: pass,
+        confirmation: confirmation
     }
 }
 
@@ -41,7 +45,10 @@ const displaySignUpStatus = async () => {
     if (obj.name === '' | obj.email === '' | obj.pass === '') {
         alert.innerHTML = `<p class="alert">未入力の項目があります。</p>`
         return false;
-    } 
+    } else if(obj.pass != obj.confirmation){
+        alert.innerHTML = `<p class="confirmation">パスワードが一致しません</p>`
+        return false;
+    }
 
     postFlg = await PostSignUp(obj)
     console.log(postFlg.status_flg)
