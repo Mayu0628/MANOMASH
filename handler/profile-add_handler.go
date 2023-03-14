3da9e3b5c2926b9874d9cda52ea80a04e262c556
mahiro <c1102864@planet.kanazawa-it.ac.jp>
2023-03-15 02:45:56 +0900

    a

--- handler/profile-add_handler.go
+++ handler/profile-add_handler.go
@@ -6,21 +6,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"time"
 	"encoding/json"
 )
-
-type AddOshiData struct{
-		OshiID		int
-		Id			uint
-		OshiName	string
-		Birthday	time.Time
-		LikePoint1	string
-		LikePoint2	string
-		LikePoint3	string
-		FreeSpace	string
-		Interest	string
-	}
 
 func ProfileAddHandler(w http.ResponseWriter, req *http.Request) {
 	id, err := strconv.Atoi(req.URL.Query().Get("id"))
@@ -35,10 +22,20 @@ func ProfileAddHandler(w http.ResponseWriter, req *http.Request) {
 		json.NewEncoder(w).Encode(ResData)
 		return
 	}
-	var SetOshiData AddOshiData
-	database.DB.First(&SetOshiData, "user_id = ?", id)
-	database.DB.Create(&reqOshiData)
-	ResData := ResFlgCreate(1, "succesful", SetOshiData.Id)
+	addDate := model.Oshi{
+		OshiName: reqOshiData.OshiName,
+		Birthday: reqOshiData.Birthday,
+		OshiMeet: reqOshiData.OshiMeet,
+		LikePoint1:reqOshiData.LikePoint1,
+		LikePoint2:reqOshiData.LikePoint2,
+		LikePoint3:reqOshiData.LikePoint3,
+		Free_space:reqOshiData.Free_space,
+		Interest: reqOshiData.Interest,
+	}
+	var SendID model.User
+	database.DB.First(&SendID, "user_id = ?", id)
+	database.DB.Create(&addDate)
+	ResData := ResFlgCreate(1, "succesful", SendID.Id)
 	if err := json.NewEncoder(w).Encode(ResData); err != nil {
 		fmt.Println(err)
 		ResData := ResFlgCreate(0, "fail", 0)
